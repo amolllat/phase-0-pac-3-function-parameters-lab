43714f11f0a22fea741d5889f184c8be773a9330
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-25 15:27:49 +0300

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function introduction(name){
+  return `Hi, my name is ${name}.`;
+}
+    console.log(introduction("Aki"));
+    console.log(introduction("Samip"));
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Aki", "Ember.js"));
+console.log(introductionWithLanguage("Samip", "React"));
+
+ 
+function introductionWithLanguageOptional(firstName="Gracie", language="JavaScript") {
+    return `Hi, my name is ${firstName} and I am learning to program in ${language}.`
+}
+    console.log( introductionWithLanguageOptional("Gracie","JavaScript"));
+
+    function introductionWithLanguageOptional(firstName, language="JavaScript") {
+        return `Hi, my name is ${firstName} and I am learning to program in ${language}.`
+    }
+    console.log( introductionWithLanguageOptional("Gracie", "Python"));    
+    
